fa575f7037ebc55091379d18f7619e712b5df0cc
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-31 15:46:14 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116012826) do
+ActiveRecord::Schema.define(version: 20180131104430) do
 
   create_table "movies", force: :cascade do |t|
     t.string  "title"
